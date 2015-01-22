2f9e84837fb76a058894a199be5812de6fc6259c
Karen S Kim <karenskim19@gmail.com>
2015-01-21 17:04:23 -0800

    add new user view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121032404) do
+ActiveRecord::Schema.define(version: 20150122004256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150121032404) do
     t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_confirmation"
   end
 
 end
