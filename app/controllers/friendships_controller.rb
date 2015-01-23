9d5017481c382b6c0d4a4e0f7ec1bb7ed0a47ec9
Zillah Chen <zillah_c@hotmail.com>
2015-01-23 14:47:47 -0800

    able to delete friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,15 +3,16 @@ class FriendshipsController < ApplicationController
     # @friendship = Friendship.new({:friendee_id => params[:friendee_id], :user_id => current_user.id})
     @friendship = current_user.friendships.build({:friendee_id => params[:friendee_id], :user_id => current_user.id})
     if @friendship.save
-      flash[:notice] = "added friend"
       redirect_to users_path
     else
-      flash[:notice] = "not able to add"
       redirect_to users_path
     end
 
   end
 
   def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    redirect_to users_path
   end
 end
