b7d7435c4b601ed21f4c3a644a1330d265f4c7bd
Zillah Chen <zillah_c@hotmail.com>
2015-01-26 15:52:05 -0800

    create accept friendships creates only 1 record

--- app/controllers/friendship_requests_controller.rb
+++ app/controllers/friendship_requests_controller.rb
@@ -1,5 +1,6 @@
 class FriendshipRequestsController < ApplicationController
   def create
+    
     @request = current_user.friendship_requests.build({:friendee_id => params[:friendee_id], :user_id => current_user.id})
     if @request.save
       redirect_to users_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
 
 
 
-  has_many :friendships
-  has_many :friends, through: :friendships
+  has_many :friendships, inverse_of: :user
+  has_many :friends, through: :friendships, inverse_of: :friendee
 
 
 
