12fd8b1814dc9a022f8eb3199e2b25dd40251e77
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-01-25 17:04:51 -0800

    made changes to handle carrierwave and postgress sitll needs more work though to get images to show

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,8 +8,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  # storage :fog
-  storage :postgresql_lo
+  storage :fog
+  # storage :postgresql_lo
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
