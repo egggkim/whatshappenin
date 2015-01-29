46f2e4dcce83f8343f87909d8b0caa0834d9662e
Zillah Chen <zillah_c@hotmail.com>
2015-01-28 19:53:22 -0800

    add gitignore

--- app/controllers/friendship_requests_controller.rb
+++ app/controllers/friendship_requests_controller.rb
@@ -1,7 +1,13 @@
 class FriendshipRequestsController < ApplicationController
+
+
+
   def create
     
     @request = current_user.friendship_requests.build({:friendee_id => params[:friendee_id], :user_id => current_user.id})
+
+
+
     if @request.save
       redirect_to users_path
     else 
