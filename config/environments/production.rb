da24f29ddf1731f2206f238f8d1de608c73595fa
Karen S Kim <karenskim19@gmail.com>
2015-02-02 09:28:47 -0800

    add precompile env to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  RAILS_ENV=production rake assets:precompile
 
   # Generate digests for assets URLs.
   config.assets.digest = true
